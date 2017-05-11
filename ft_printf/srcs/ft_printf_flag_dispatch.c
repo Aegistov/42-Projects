061bb581fc211a75aa6ce265e35204c7b855015d
Michael J <mmorel@e1z2r9p33.42.us.org>
2017-05-11 15:00:57 -0700

    More work on dispatch function

--- ft_printf/srcs/ft_printf_flag_dispatch.c
+++ ft_printf/srcs/ft_printf_flag_dispatch.c
@@ -10,12 +10,19 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+char	(*flag_minus)(const char *restrict);
+char	(*flag_plus)(const char *restrict);
+char	(*flag_zero)(const char *restrict);
+char	(*flag_space)(const char *restrict);
+char	(*flag_hash)(const char *restrict);
+
 void	ft_printf_flag_dispatch(const char *restrict format, char *flags)
 {
-	char (*flag_functions[6])(const char *restrict)
-	char (*flag_minus)(const char *restrict)
-	char (*flag_plus)(const char *restrict)
-	char (*flag_zero)(const char *restrict)
-	char (*flag_space)(const char *restrict)
-	char (*flag_hash)(const char *restrict)
+	char (*flag_functions[5])(const char *restrict)
+	flag_functions[0] = char (*flag_minus)(const char *restrict);
+	flag_functions[1] = char (*flag_plus)(const char *restrict);
+	flag_functions[2] = char (*flag_zero)(const char *restrict);
+	flag_functions[3] = char (*flag_space)(const char *restrict);
+	flag_functions[4] = char (*flag_hash)(const char *restrict);
+	flag_functions[5] = '\0';
 }
