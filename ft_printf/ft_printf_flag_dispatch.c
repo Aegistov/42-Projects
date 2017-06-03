6d9de8f6da655186f381c3ea6030056fd72beddb
Michael J <mmorel@e1z2r1p1.42.us.org>
2017-06-03 02:02:53 -0700

    Back to working version. Next is width

--- ft_printf/ft_printf_flag_dispatch.c
+++ ft_printf/ft_printf_flag_dispatch.c
@@ -12,6 +12,15 @@
 
 #include "libftprintf.h"
 
+
+void		pf_string_init(t_pf_string *str)
+{
+	str->pad = NULL;
+	str->len = 0;
+	str->neg = 0;
+	str->num_str = NULL;
+}
+
 intmax_t	ft_printf_cast(t_mods *mod, va_list insertion)
 {
 	intmax_t	holder;
@@ -84,7 +93,7 @@ int		precision_check(char *str, int precision)
 	return (len);
 }
 
-char	*padding_for_num(t_mods *mod, int len, int sign)
+char	*padding_for_num(t_mods *mod, t_pf_string *nbr)
 {
 	char	*pad;
 	int		flag_sign;
@@ -138,65 +147,48 @@ char	*padding(t_mods *mod, int len, int sign)
 	return (pad);
 }
 
-// int		ft_printf_hhd(va_list insertion, char *flags, int width, int precision)
-// {
-// 	int		count;
-// 	int		holder;
-
-// 	// if ()
-// 	holder = va_arg(insertion, signed char);
-// 	printf("I'm using them! %s%d%d\n", flags, width, precision);
-// 	count = 0;
-
-// 	return (count);
-// }
-
 int		ft_printf_di(va_list insertion, t_mods *mod)
 {
-	intmax_t 	holder;
-	char	*number_str;
-	char	*pad;
-	int		len;
+	t_pf_string	nbr;
 	int		index;
-	int 	sign;
 	int		count;
 
-	// pad = NULL;
-	len = 0;
+	pf_string_init(&nbr);
 	index = -1;
-	holder = ft_printf_cast(mod, insertion);
-	sign = 0;
+	nbr.arg.mint = ft_printf_cast(mod, insertion);
 	count = 0;
-	if (holder < 0)
+	if (nbr.arg.mint < 0)
 	{
-		sign = 1;
-		if (holder != -2147483648)
-			holder *= -1;
+		nbr.neg = 1;
+		if (nbr.arg.mint != -2147483648)
+			nbr.arg.mint *= -1;
 	}
-	number_str = ft_itoa(holder);
-	// printf("Number: %s\n", number_str);
-	len = precision_check(number_str, mod->precision);
-	pad = padding_for_num(mod, len, sign);
-	count = integer_signage(mod, sign, pad, holder);
-	if (!mod->left_align && pad)
-		ft_putstr_fd(pad, 1);
-	if (sign == 1 && pad && pad[0] == ' ')
+	nbr.num_str = ft_itoa(nbr.arg.mint);
+	nbr.len = precision_check(nbr.num_str, mod->precision);
+	nbr.pad = padding_for_num(mod, nbr);
+	count = integer_signage(mod, nbr.neg, nbr.pad, nbr.arg.mint);
+	if (!mod->left_align && nbr.pad)
+		ft_putstr_fd(nbr.pad, 1);
+	if (nbr.neg == 1 && nbr.pad && nbr.pad[0] == ' ')
 		{
 			ft_putchar_fd('-', 1);
 			count++;
 		}
-	while (number_str[++index] != '\0' && index < len)
-		ft_putchar_fd(number_str[index], 1);
-	if(mod->left_align && pad)
+	while (nbr.num_str[++index] != '\0' && index < nbr.len)
+		ft_putchar_fd(nbr.num_str[index], 1);
+	if(mod->left_align && nbr.pad)
 	{
 		if (mod->zero)
 		{
-			free(pad);
-			ft_strfill(pad, ' ', mod->width);
+			free(nbr.pad);
+			ft_strfill(nbr.pad, ' ', mod->width);
 		}
-		ft_putstr_fd(pad, 1);
+		ft_putstr_fd(nbr.pad, 1);
 	}
-	return (len + mod->width + count);	
+	if (mod->precision > mod->width)
+		return (nbr.len + mod->precision + count);
+	else
+		return (nbr.len + mod->width + count);	
 }
 
 // void	ft_printf_o(va_list insertion)
@@ -214,82 +206,54 @@ int		ft_printf_di(va_list insertion, t_mods *mod)
 
 int		ft_printf_c(va_list insertion, t_mods *mod)
 {
-	char	*pad;
-	char	c;
-	int		len;
+	t_pf_string	chr;
 	int		index;
 	int		width;
 
 	width = mod->width;
-	pad = NULL;
-	c = va_arg(insertion, int);
-	len = 1;
+	pf_string_init(&chr);
+	chr.arg.ch = va_arg(insertion, int);
+	chr.len = 1;
 	index = -1;
-	if (!c)
-		c = 0;
-	if (width - len > 0)
+	if (!chr.arg.ch)
+		chr.arg.ch = 0;
+	if (width - chr.len > 0)
 	{
-		mod->width -= len;
-		pad = ft_strfill(pad, ' ', mod->width);
+		mod->width -= chr.len;
+		chr.pad = ft_strfill(chr.pad, ' ', mod->width);
 	}
-	if (!mod->left_align && pad)
-		ft_putstr_fd(pad, 1);
-	ft_putchar_fd(c, 1);
-	if(mod->left_align && pad)
-		ft_putstr_fd(pad, 1);
-	return (len + mod->width);
+	if (!mod->left_align && chr.pad)
+		ft_putstr_fd(chr.pad, 1);
+	ft_putchar_fd(chr.arg.ch, 1);
+	if(mod->left_align && chr.pad)
+		ft_putstr_fd(chr.pad, 1);
+	return (chr.len + mod->width);
 }
 
 int		ft_printf_s(va_list insertion, t_mods *mod)
 {
-	// char	*pad;
-	// char	*str;
-	int		len;
 	t_pf_string	str;
 	int			index;
 	int			width;
 
-	// if (!(str = (t_pf_string *)ft_memalloc(sizeof(t_pf_string))))
-	// 	return (-1);
 	width = mod->width;
-	len = str.len;
+	pf_string_init(&str);
 	str.arg.str = va_arg(insertion, char *);
-	printf("String: %s\n", str.arg.str);
 	index = -1;
-	printf("null check\n");
 	if (!str.arg.str)
 		str.arg.str = "(null)";
-	printf("len loading\n");
 	str.len = precision_check(str.arg.str, mod->precision);
-	printf("LEN: %d\nWidth: %d\nWidth - len = %d\n", len, mod->width, mod->width - len);
 	if (width - str.len > 0)
 	{
-		printf("Editing width: %d\n", mod->width);
 		mod->width -= str.len;
-		// printf("%d\n", mod->width);
 		str.pad = ft_strfill(str.pad, ' ', mod->width);
 	}
-	printf("pad check\n");
-	if (str.pad == NULL)
-		printf("NULL");
-	printf("next stage: %s\n", str.pad);
 	if (!mod->left_align && str.pad != NULL)
-	{
-		printf("entered\n");
-		// p
 		ft_putstr_fd(str.pad, 1);
-		printf("complete\n");
-	}
-	printf("arguement printing\n");
-	while (str.arg.str[++index] != '\0')
-	{
-		// printf("Char: %c\n", str.arg.str[index]);
+	while (str.arg.str[++index] != '\0' && index < str.len)
 		ft_putchar_fd(str.arg.str[index], 1);
-	}
-	// printf("Char out of loop: %c\n", str.arg.str[index]);
 	if(mod->left_align && str.pad)
 		ft_putstr_fd(str.pad, 1);
-	// printf("LEN: %d\nWidth: %d\n", len, mod->width);
 	return (str.len + mod->width);
 }
 
@@ -318,13 +282,13 @@ int		ft_printf_flag_dispatch(t_mods *mod, va_list insertion, int argument)
 {
 	int (*argument_list[127])(va_list, t_mods *);
 	int	len;
-	// argument_list['d'] = ft_printf_di;
-	// argument_list['i'] = ft_printf_di;
+	argument_list['d'] = ft_printf_di;
+	argument_list['i'] = ft_printf_di;
 	// argument_list['h'] = ft_printf_hhd;
 	// argument_list['o'] = ft_printf_o;
 	// argument_list['x'] = ft_printf_x;
 	// argument_list['u'] = ft_printf_di;
-	// argument_list['c'] = ft_printf_c;
+	argument_list['c'] = ft_printf_c;
 	argument_list['s'] = ft_printf_s;
 	// argument_list['f'] = ft_printf_f;
 	// argument_list['e'] = ft_printf_e;
